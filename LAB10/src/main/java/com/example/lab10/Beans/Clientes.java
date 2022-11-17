a0244ae6be35e196176572f0e6b9dead484ac445
noe <noe.jaraq@gmail.com>
2022-11-16 20:19:10 -0500

    cliente

---
+++ LAB10/src/main/java/com/example/lab10/Beans/Clientes.java
@@ -0,0 +1,61 @@
+package com.example.lab10.Beans;
+
+public class Clientes {
+
+    private String nombreCliente;
+    private String edad;
+    private String tipoCliente;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private int cantidadDeContactos;
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getCantidadDeContactos() {
+        return cantidadDeContactos;
+    }
+
+    public void setCantidadDeContactos(int cantidadDeContactos) {
+        this.cantidadDeContactos = cantidadDeContactos;
+    }
+
+
+}
